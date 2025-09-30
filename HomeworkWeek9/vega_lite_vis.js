cf88df4a617c97547493b381d79244f98c3c6d62
Brittany Lau <blau0015@student.monash.edu>
2025-09-30 16:59:13 +0800

    changed to vg.json file

--- HomeworkWeek9/vega_lite_vis.js
+++ HomeworkWeek9/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "HomeworkWeek9.json";
+var vg_1 = "HomeworkWeek9.vg.json";
 vegaEmbed("#choropleth", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
