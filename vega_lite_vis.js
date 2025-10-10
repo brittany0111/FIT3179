b07243df17e3de278d1b7cbda213c33dd02e93a0
Brittany Lau <blau0015@student.monash.edu>
2025-10-10 13:03:41 +0800

    line chart

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "HomeworkWeek9.vg.json";
 vegaEmbed("#choropleth", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "W10/HomeworkWeek10.vg.json";
+vegaEmbed("#bar", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
