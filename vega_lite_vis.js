65011dfcacb1408acecb001b5f691462bdb604a1
Brittany Lau <blau0015@student.monash.edu>
2025-10-10 13:43:09 +0800

    f

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,5 @@
 var vg_1 = "HomeworkWeek9.vg.json";
-vegaEmbed("#choropleth", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+vegaEmbed("#choropleth", vg_1).then(console.log).catch(console.error);
 
 var vg_2 = "HomeworkWeek10.vg.json";
-vegaEmbed("#line", vg_2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+vegaEmbed("#line", vg_2).then(console.log).catch(console.error);
