d765f90c32ec8c7905e1f18224c7d8088aa0acfe
Brittany Lau <blau0015@student.monash.edu>
2025-10-20 15:11:04 +0800

    donut

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -7,8 +7,8 @@ vegaEmbed("#line", vg_2).then(console.log).catch(console.error);
 var vg_3 = "BumpChart.vg.json";
 vegaEmbed("#bump", vg_3).then(console.log).catch(console.error);
 
-var vg_4 = "StackedArea.vg.json";
-vegaEmbed("#stacked_area", vg_4).then(console.log).catch(console.error);
+var vg_4 = "DonutChart.vg.json";
+vegaEmbed("#donut", vg_4).then(console.log).catch(console.error);
 
 var vg_5 = "ProportionalSymbolMap.vg.json";
 vegaEmbed("#prop_symbol", vg_5).then(console.log).catch(console.error);
